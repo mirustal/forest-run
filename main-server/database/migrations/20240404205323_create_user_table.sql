e6e2f6132012927d36abb6a8746afdecf6afcd6b
solidius <49648821+solidsign@users.noreply.github.com>
2024-04-05 23:34:30 +0300

    add create user table migration

--- main-server/database/migrations/20240404205323_create_user_table.sql
+++ main-server/database/migrations/20240404205323_create_user_table.sql
@@ -1,9 +1,18 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS users
+(
+    id SERIAL PRIMARY KEY ,
+    login VARCHAR(64) NOT NULL UNIQUE,
+    password CHAR(256) NOT NULL,
+    name varchar(64),
+    avatar_url text,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE IF EXISTS users;
 -- +goose StatementEnd
