5b5b38a4ab5de3e7bb44aea8d872634a3b8565ff
solidius <49648821+solidsign@users.noreply.github.com>
2024-04-06 00:10:47 +0300

    add create runs table

---
+++ main-server/database/migrations/20240405205611_create_runs_table.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS runs
+(
+    id                    SERIAL       NOT NULL PRIMARY KEY,
+    name                  VARCHAR(128) NOT NULL,
+    description           TEXT,
+    official_site         TEXT,
+    avatar_url            TEXT,
+    route                 json         NOT NULL,
+    start_time            TIMESTAMP    NOT NULL,
+    start_place           TEXT         NOT NULL,
+    start_place_latitude  TEXT         NOT NULL,
+    start_place_longitude TEXT         NOT NULL,
+    max_participants      INT          NOT NULL,
+    is_photo_allowed      bool         NOT NULL DEFAULT TRUE,
+    is_stories_allowed    bool         NOT NULL DEFAULT TRUE,
+    status                int4         NOT NULL DEFAULT 0,
+    participation_format  int4         NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS runs;
+-- +goose StatementEnd
