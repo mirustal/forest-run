f6a3ef21991f1fed750e46f7469f3ac89cddc549
solidius <49648821+solidsign@users.noreply.github.com>
2024-04-06 00:16:09 +0300

    add create runs participations table migration

---
+++ main-server/database/migrations/20240405211106_create_runs_participation_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS runs_participations
+(
+    participant_id INT NOT NULL REFERENCES users (id),
+    run_id         INT NOT NULL REFERENCES runs (id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop TABLE IF EXISTS runs_participations;
+-- +goose StatementEnd
