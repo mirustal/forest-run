f6fec32a920cc05c0f7f2b0967efa3dea0baec9c
solidius <49648821+solidsign@users.noreply.github.com>
2024-04-06 00:15:42 +0300

    use integers for id

--- main-server/database/migrations/20240405203859_create_subscriptions_table.sql
+++ main-server/database/migrations/20240405203859_create_subscriptions_table.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS subscriptions
 (
-    follower_id BIGINT NOT NULL REFERENCES users (id),
-    followed_id BIGINT NOT NULL REFERENCES users (id)
+    follower_id INT NOT NULL REFERENCES users (id),
+    followed_id INT NOT NULL REFERENCES users (id)
 );
 -- +goose StatementEnd
 
