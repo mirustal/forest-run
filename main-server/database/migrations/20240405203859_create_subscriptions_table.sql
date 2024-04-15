cf8df74404c481d75e2f704ad9e3947927a185c8
solidius <49648821+solidsign@users.noreply.github.com>
2024-04-15 22:30:36 +0300

    fix multiple subscriptions

--- main-server/database/migrations/20240405203859_create_subscriptions_table.sql
+++ main-server/database/migrations/20240405203859_create_subscriptions_table.sql
@@ -3,7 +3,8 @@
 CREATE TABLE IF NOT EXISTS subscriptions
 (
     follower_id INT NOT NULL REFERENCES users (id),
-    followed_id INT NOT NULL REFERENCES users (id)
+    followed_id INT NOT NULL REFERENCES users (id),
+    PRIMARY KEY (follower_id, followed_id)
 );
 -- +goose StatementEnd
 
